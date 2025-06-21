6d4e9b270e2cc9c777c9c4eba766fc2f27f19bee
Sara Machado <sarasanto1309@gmail.com>
2025-06-21 13:59:12 -0300

    Created interactive menu, ABC class, adjusted assets and created parallax effect

--- code/const.py
+++ code/const.py
@@ -7,7 +7,13 @@ COLOR_PINK = 229, 82, 242
 MENU_OPTION = ('NEW GAME',
                'SCORE',
                'EXIT')
+#S
+ENTITY_SPEED = {
+    'level1BG0': 0,
+    'level1BG1': 1,
+    'level1BG2': 2
+}
 
 # W
 WIN_WIDTH = 600
-WIN_HEIGHT = 400
+WIN_HEIGHT = 480
--- code/menu.py
+++ code/menu.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from json.encoder import py_encode_basestring_ascii
+
 import pygame.image
 from pygame.font import Font
 from pygame.rect import Rect
@@ -14,18 +16,24 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        menu_option = 0
         pygame.mixer_music.load('./assets/menu.mp3')
         pygame.mixer_music.play(-1)
         while True:
+            # DRAW IMAGES
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(52, 'Fairy', COLOR_CREAM, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, 'Fairy', COLOR_PURPLE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(52, 'Shooter', COLOR_CREAM, ((WIN_WIDTH / 2), 120))
-            self.menu_text(50, 'Shooter', COLOR_PURPLE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(52, 'Fairy', COLOR_CREAM, ((WIN_WIDTH / 2), 110))
+            self.menu_text(50, 'Fairy', COLOR_PURPLE, ((WIN_WIDTH / 2), 110))
+            self.menu_text(52, 'Shooter', COLOR_CREAM, ((WIN_WIDTH / 2), 180))
+            self.menu_text(50, 'Shooter', COLOR_PURPLE, ((WIN_WIDTH / 2), 180))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(26, MENU_OPTION[i], COLOR_CREAM, ((WIN_WIDTH / 2), 250 + 30 * i))
-                self.menu_text(25, MENU_OPTION[i], COLOR_PINK, ((WIN_WIDTH / 2), 250 + 30 * i))
+                if i == menu_option:
+                    self.menu_text(26, MENU_OPTION[i], COLOR_CREAM, ((WIN_WIDTH / 2), 300 + 30 * i))
+                    self.menu_text(25, MENU_OPTION[i], COLOR_PURPLE, ((WIN_WIDTH / 2), 300 + 30 * i))
+                else:
+                    self.menu_text(26, MENU_OPTION[i], COLOR_CREAM, ((WIN_WIDTH / 2), 300 + 30 * i))
+                    self.menu_text(25, MENU_OPTION[i], COLOR_PINK, ((WIN_WIDTH / 2), 300 + 30 * i))
 
             pygame.display.flip()
 
@@ -34,6 +42,22 @@ class Menu:
                 if event.type == pygame.QUIT:
                     pygame.quit()  # Close Window
                     quit() # End pygame
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN: # DOWN KEY
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+
+                    if event.key == pygame.K_UP: # UP KEY
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+
+                    if event.key == pygame.K_RETURN: # ENTER
+                        return MENU_OPTION[menu_option]
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.Font("assets/fonts/PressStart2P-Regular.ttf", size=text_size)
